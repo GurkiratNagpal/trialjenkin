882bc66f02c4c806667de497a816f8c17f3d0622
Debadwar <Abhijeet.Debadwar@team.telstra.com>
2021-09-13 20:44:36 +0530

    mysql_in_resources

--- src/main/java/com/auth/Signing/SigningApplication.java
+++ src/main/java/com/auth/Signing/SigningApplication.java
@@ -7,8 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SigningApplication {
 
+	Co
+
 	public static void main(String[] args) {
 		SpringApplication.run(SigningApplication.class, args);
+
 	}
 
 }
